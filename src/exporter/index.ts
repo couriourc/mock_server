ae81cd842581e5ff2f0627dba55b6eba125402b3
godakid <godakid@outlook.com>
2024-09-13 22:44:59 +0800

    example: add example of mock

---
+++ src/exporter/index.ts
@@ -0,0 +1,5 @@
+export {startup} from "../core/startup";
+export {getPlugins,} from "../core/plugin-handler";
+export {config} from "../core/config";
+export {extractFunctionOrValue} from "../utils/index";
+export {getLogger} from "../utils/logger";
